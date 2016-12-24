e976db5bf881a11df92821b1bfa59c51a14db287
ZhangJin <nbnbnb.zhang@gmail.com>
2016-12-24 17:30:25 +0800

    添加项目文件。

---
+++ src/App/Program.cs
@@ -0,0 +1,16 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"The answer is {new Thing().Get(42)}");
+        }
+    }
+}
