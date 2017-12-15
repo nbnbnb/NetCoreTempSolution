1b9bdff2f88db2240c869a487dcb6cf17ff2242f
张进 <jinzhanga@Ctrip.com>
2017-12-15 19:53:20 +0800

    cc

--- ConsoleAppCore/Demos/CSharp72Features.cs
+++ ConsoleAppCore/Demos/CSharp72Features.cs
@@ -53,30 +53,43 @@ namespace ConsoleAppCore.Demos
         /// C# 7.2 新增 in 关键字来补充现有的 ref 和 out 关键字
         /// in 关键字指定要按引用传递参数，并且调用的方法不会修改传递给它的值
         /// </summary>
-        private static double InParam(in Point point1, in Point point2)
+        private static void InParam(in MyPoint myPoint, in ReadOnlyMyPoint readOnlyMyPoint)
         {
-            double xDifference = point1.X - point2.X;
-            double yDifference = point1.Y - point2.Y;
-
             // 无法进行赋值
-            // point1.X = 123;
-
-            // ref readonly
-            // 无法进行赋值
-            // Origin.X = 123;
-
-            // 还可以声明一个 ref readonly 变量
-            // 右侧需要用 ref 声明
-            ref readonly Point originReference = ref origin;
-
-            return Math.Sqrt(xDifference * xDifference + yDifference * yDifference);
+            // myPoint.X = 123;
+            //readOnlyMyPoint.X = 123;
         }
 
-        private static Point origin = new Point(1, 2);
-        public static ref readonly Point Origin => ref origin;
+        private static ReadOnlyMyPoint readOnlyMyPoint = new ReadOnlyMyPoint();
+        private static MyPoint myPoint = new MyPoint();
+
+        public static void Test()
+        {
+            // 将 readonly struct 作为 in 参数传递到其成员方法
+            InParam(myPoint, readOnlyMyPoint);
+        }
+
+        /// <summary>
+        /// ref struct 声明，指示结构类型直接访问托管的内存（栈）
+        /// ref struct 声明，指示结构类型直接访问托管的内存，且必须始终由堆栈分配
+        /// 
+        /// ref struct 的限制
+        ///     1，不能对 ref struct 装箱。 无法向属于 object、dynamic 或任何接口类型的变量分配 ref struct 类型。
+        ///     2，不能将 ref struct 声明为类或常规结构的成员。
+        ///     3，不能声明异步方法中属于 ref struct 类型的本地变量。 不能在返回 Task、Task<T> 或类似 Task 类型的同步方法中声明它们。
+        ///     4，无法在迭代器中声明 ref struct 本地变量。
+        ///     5，无法捕获 Lambda 表达式或本地函数中的 ref struct 变量。
+        ///   
+        /// </summary>
+        private static ref MyPoint GetRefMyPoint()
+        {
+            return ref myPoint;
+        }
 
         private class XYZ
         {
+            public int Z { get; set; }
+
             /// <summary>
             /// 新增的 private protected 
             /// private || protected
@@ -107,29 +120,36 @@ namespace ConsoleAppCore.Demos
 
         /// <summary>
         /// 使用 readonly struct 声明
-        /// 调用 readonly struct 的成员时，编译器将生成更高效的代码：this 引用（in 参数功能）
-        /// 这种优化在你使用 readonly struct 时可减少更多复制操作。
+        /// 调用 readonly struct 的成员时，编译器将生成更高效的代码：this 引用
+        /// 这种优化在你使用 readonly struct 时可减少更多复制操作
         /// 
-        /// 存在针对 readonly struct 的其他优化。 
-        /// 可在 readonly struct 为参数的每个位置使用 in 修饰符。
-        /// 此外，如果要返回其生存期超出返回对象的方法的作用域的对象，可返回 readonly struct 作为 ref return。
+        /// 存在针对 readonly struct 的其他优化
+        ///     可在 readonly struct 为参数的每个位置使用 in 修饰符
+        ///     此外，如果要返回其生存期超出返回对象的方法的作用域的对象，可返回 readonly struct 作为 ref return
         /// </summary>
-        public readonly struct ReadonlyPoint3D
+        private readonly struct ReadOnlyMyPoint
         {
-            public ReadonlyPoint3D(double x, double y, double z)
+            public ReadOnlyMyPoint(double x, double y)
             {
                 this.X = x;
                 this.Y = y;
-                this.Z = z;
             }
-
+            // 只读结构中
+            // 属性只能设置为只读
             public double X { get; }
             public double Y { get; }
-            public double Z { get; }
+        }
 
-            private static readonly ReadonlyPoint3D origin = new ReadonlyPoint3D();
-            // 返回 readonly struct 作为 ref return
-            public static ref readonly ReadonlyPoint3D Origin => ref origin;
+        private struct MyPoint
+        {
+            public MyPoint(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public int X { get; set; }
+            public int Y { get; set; }
         }
     }
 }
