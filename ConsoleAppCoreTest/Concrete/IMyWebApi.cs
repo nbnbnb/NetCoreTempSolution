9a3062033bb8eaac3ec6c522d279acf5d0ceddf4
张进 <jinzhanga@Ctrip.com>
2018-07-10 20:48:06 +0800

    WebApiClient

---
+++ ConsoleAppCoreTest/Concrete/IMyWebApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApiClient;
+using WebApiClient.Attributes;
+
+namespace ConsoleAppCoreTest.Concrete
+{
+    [HttpHost("https://www.baidu.com/")]
+    public interface IMyWebApi : IHttpApi
+    {
+        [HttpGet]
+        ITask<string> GetContent();
+    }
+}
---
+++ ConsoleAppCoreTest/WebApiClientTest.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleAppCoreTest.Concrete;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient;
+using WebApiClient.Attributes;
+
+namespace ConsoleAppCoreTest
+{
+    [TestClass]
+    public class WebApiClientTest
+    {
+        [TestMethod]
+        public async Task BasicGetTest()
+        {
+            var client = HttpApiClient.Create<IMyWebApi>();
+            var content = await client.GetContent();
+            Assert.IsTrue(content.Contains("<!DOCTYPE html>", StringComparison.OrdinalIgnoreCase));
+        }
+
+    }
+}
