5c214452872fe84b06d8fd9089d1fdf70c6cc1c9
张进 <jinzhanga@Ctrip.com>
2018-11-01 17:02:36 +0800

    Morpher

---
+++ ConsoleAppCore/Util/Morpher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleAppCore.Util
+{    
+    public static class Morpher
+    {
+        public delegate Int32 MorpherAction<TResult, in TArgument>(Int32 startValue, TArgument argument, out TResult morphResult);
+
+        public static TResult Run<TResult, TArgument>(ref Int32 target, TArgument argument, MorpherAction<TResult, TArgument> morpher)
+        {
+            // 定义返回值
+            TResult morphResult;
+            // 定义中间变量
+            Int32 currentValue = target, startValue, desiredValue;
+            do
+            {
+                startValue = currentValue;
+
+                // 通过自定义函数计算一个期望的值
+                // 这过程中，target 的值可能改变
+                desiredValue = morpher(startValue, argument, out morphResult);
+
+                // 如果 target == startValue，表示在 morpher 这个执行期间，target 没有改变
+                // 然后则将 desiredValue 赋值给 target，同时将 target 赋值给 currentValue
+                // 此时应该是 startValue == target == currentValue
+                // 所以，将会跳出 do...while
+                currentValue = Interlocked.CompareExchange(ref target, desiredValue, startValue);
+
+                // 如果 target != startValue，表示在 morpher 这个执行期间，target 发生了改变
+                // Interlocked.CompareExchange 将返回最新的 target，然后赋值给 currentValue
+                // 所以 startValue != currentValue，将会继续 do...while
+            } while (startValue != currentValue);
+
+            return morphResult;
+        }
+    }
+
+}
--- ConsoleAppCore/Util/TaskWithCancellation.cs
+++ ConsoleAppCore/Util/TaskWithCancellation.cs
@@ -8,6 +8,14 @@ namespace ConsoleAppCore.Util
 {
     public static class TaskWithCancellation
     {
+        /// <summary>
+        /// 给 Task<T> 关联 CancellationToken
+        /// 
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="originalTask"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
         public static async Task<TResult> WithCancellation<TResult>(this Task<TResult> originalTask, CancellationToken ct)
         {
             // 创建在 CancellationToken 被取消时完成的一个 Task
@@ -16,7 +24,9 @@ namespace ConsoleAppCore.Util
             // 由于没有非泛型的 TaskCompletionSource
             // 所以此处使用一个 Int32 进行模拟返回值
 
-            // 一旦 CancellationToken 被取消，就完成 Task
+            // 一旦 CancellationToken 被取消，将会调用 Register 中的 Action
+            // 这个 Action 将会设置 CompletionSource 为完成状态
+            // 此时 await Task.WhenAny 将会返回
             using (ct.Register(t => ((TaskCompletionSource<Int32>)t).TrySetResult(0), cancelTask))
             {
                 // 创建在原始 Task 或 CancellationToken Task 完成时都完成的一个 Task
@@ -29,6 +39,7 @@ namespace ConsoleAppCore.Util
                 }
             }
 
+            // await 错误捕获方式
             // 等待原始任务（以同步方式）；若任务失败，等待它将抛出第一个内部异常，
             // 而不是抛出 AggregateException
 
@@ -54,7 +65,7 @@ namespace ConsoleAppCore.Util
 
             // 注册 CancellationToken 的取消事件
             // 一旦 CancellationToken 被取消，就将 cancelTask 置完成
-            using (ct.Register(t => ((TaskCompletionSource<Int32>)t).TrySetResult(new Int32()), cancelTask))
+            using (ct.Register(t => ((TaskCompletionSource<Int32>)t).TrySetResult(0), cancelTask))
             {
                 // 创建在原始 Task 或 CancellationToken Task 完成时都完成的一个 Task
                 Task any = await Task.WhenAny(originalTask, cancelTask.Task);
@@ -65,6 +76,7 @@ namespace ConsoleAppCore.Util
                     ct.ThrowIfCancellationRequested();
                 }
 
+                // await 错误捕获方式
                 // 等待原始任务（以同步方式）；若任务失败，等待它将抛出第一个内部一次
                 // 而不是抛出 AggregateException
 
